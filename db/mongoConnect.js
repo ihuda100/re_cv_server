89a15ade51a66e33478af8400002e479e86fbdab
ihuda100 <ihuda100@gmail.com>
2024-12-01 18:48:30 +0200

    DB

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -5,7 +5,11 @@ main().catch((err) => console.log(err));
 async function main() {
   mongoose.set("strictQuery", false);
   await mongoose.connect(
-    `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.mongodb.net/test`
+    `mongodb+srv://re_cv_server:1234@cluster0.vruq0.mongodb.net/re_cv`
+    // `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.vruq0.mongodb.net/re_cv`
+
+
+    
   );
   console.log("mongo connect");
 }
