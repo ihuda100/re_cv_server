fa36e1c9f6a750b328c08f226abb189da4f1049c
ihuda100 <ihuda100@gmail.com>
2024-12-09 19:51:39 +0200

    route resumes

--- routes/resumes.js
+++ routes/resumes.js
@@ -6,12 +6,15 @@ router.get("/", (req, res, next) => {
   res.json({ msg: "Work from resumes.js" });
 });
 
-// router.post("/", (req, res, next) => {
-    
-//     let data = gptnnn(req.pdfnn)
-//   res.json(data);
-// });
-
-
+router.post("/", async (req, res, next) => {
+  try {
+    let data = await convertPDFToJson(req.pdf);
+    let upgrateData = await cvUpgrade(data);
+    await upgrateData.save();
+    res.json(upgrateData);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = router;
