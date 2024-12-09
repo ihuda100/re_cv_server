e5ddf66fa52b09d86d23125587063b80b2a0dafa
shimon driham <95618903+shimondriham@users.noreply.github.com>
2024-12-10 00:37:22 +0200

    resume , ResumeModel

---
+++ models/resumeModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const resumeSchema = new mongoose.Schema({
+    _idUser: String,
+    FullName: String,
+    email: String,
+    pone: String,
+    ifUpdate: {
+        type: Boolean, default: false
+    },
+});
+exports.ResumeModel = mongoose.model("resumes", resumeSchema);
+
+
+exports.validResume = (_bodyData) => {
+    let joiSchema = Joi.object({
+        FullName: Joi.string().min(5).max(30).required(),
+        email: Joi.string().min(5).max(30).email().required(),
+        pone: Joi.string().min(10).max(15).required(),
+    });
+    return joiSchema.validate(_bodyData);
+};
--- routes/resumes.js
+++ routes/resumes.js
@@ -1,4 +1,6 @@
 var express = require("express");
+const { ResumeModel, validResume } = require("../models/resumeModel");
+const { auth } = require("../middlewares/auth");
 var router = express.Router();
 
 /* GET home page. */
@@ -6,15 +8,56 @@ router.get("/", (req, res, next) => {
   res.json({ msg: "Work from resumes.js" });
 });
 
-router.post("/", async (req, res, next) => {
+
+// Convert from PDF to json
+router.post("/convert", async (req, res, next) => {
   try {
-    let data = await convertPDFToJson(req.pdf);
-    let upgrateData = await cvUpgrade(data);
-    await upgrateData.save();
-    res.json(upgrateData);
+    let data = await convertPDFToJson(req.body.pdf);
+    res.status(200).json(data);
   } catch (err) {
     console.log(err);
   }
 });
+
+
+// Upgrades the resume
+router.post("/upgrade", auth, async (req, res, next) => {
+  let validBody = validResume(req.body);
+  if (validBody.error) {
+    return res.status(400).json(validBody.error.details);
+  }
+  try {
+    let _idUser = req.userToken.id;
+    let data = req.body;
+    let upgrateData = await cvUpgrade(data);
+    let resume = new ResumeModel(upgrateData);
+    resume._idUser = _idUser
+    await resume.save();
+    res.status(201).json(resume);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+// Updates the resume after change and approval
+router.post("/update", async (req, res, next) => {
+  let validBody = validResume(req.body);
+  if (validBody.error) {
+    return res.status(400).json(validBody.error.details);
+  }
+  try {
+    let data = req.body;
+    let resume = await ResumeModel.findOne({ id: data._id });
+    resume = data;
+    resume.ifUpdate = true;
+    let updateData = await ResumeModel.updateOne({ _id: resume._id }, resume);
+    res.status(200).json(updateData);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
 
 module.exports = router;
