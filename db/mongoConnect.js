40f53ef420a27ace67516df31e292394f39e0c95
shimon driham <95618903+shimondriham@users.noreply.github.com>
2024-12-16 01:43:38 +0200

    aa

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -7,8 +7,6 @@ async function main() {
   await mongoose.connect(
     // `mongodb+srv://re_cv_server:1234@cluster0.vruq0.mongodb.net/re_cv`
     `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.vruq0.mongodb.net/re_cv`
-
-
   );
   console.log("mongo connect");
 }
