0559948003a6ddda5886ee4d4f384afdf1225eac
ihuda100 <ihuda100@gmail.com>
2025-01-25 22:56:08 +0200

    resumes model update date

--- models/resumeModel.js
+++ models/resumeModel.js
@@ -22,6 +22,7 @@ exports.validResume = (_bodyData) => {
     _id: Joi.string().optional(),
     __v: Joi.number().optional(),
     _idUser: Joi.string().optional(),
+    dateCreated: Joi.string().optional(),
     fullName: Joi.string().min(5).max(30).required(),
     email: Joi.string().min(5).max(30).email().required(),
     phone: Joi.string().min(10).max(15).required(),
